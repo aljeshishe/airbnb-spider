6adbb8a92cb966a1da05aac70877f63259c09467
alexeygrachev <agrachev@cindicator.com>
2023-01-06 21:25:07 +0400

    added run_spider.py

---
+++ airbnb_spider/run_spider.py
@@ -0,0 +1,13 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from airbnb_spider.spiders.airbnb import AirbnbSpider
+
+
+def run_spider():
+    process = CrawlerProcess(settings=get_project_settings())
+    process.crawl(AirbnbSpider, params=AirbnbSpider.DEFAULT_PARAMS)
+    process.start()
+
+if __name__ == "__main__":
+    run_spider()
