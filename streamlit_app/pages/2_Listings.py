0026bf442f872f2c67b70aadcfb6a8536d0536ab
alexeygrachev <agrachev@cindicator.com>
2023-01-07 16:58:26 +0400

    added StaysPdpSections requests to experiments

--- streamlit_app/pages/2_Listings.py
+++ streamlit_app/pages/2_Listings.py
@@ -1,12 +1,12 @@
 import plotly.express as px
 import streamlit as st
 
-from airbnb import data, chart
+from airbnb import data, listings_chart
 
 px.set_mapbox_access_token(
     "pk.eyJ1IjoiYWxqZXNoaXNoZSIsImEiOiJjbGJwaHJiajMwc3pxM3FxaDc4anltemN1In0.XZoRGDAvlJjbC21qfZTJcA")
 
 df = data.load("../results/2022-12-21_02-59-19")
-fig = chart.create_chart(df)
+fig = listings_chart.create(df)
 st.markdown("# Airbnb listings")
 st.plotly_chart(fig)
