83ee8e3404bfc1968dbbe995cd0ef9b094763fc3
alexeygrachev <agrachev@cindicator.com>
2023-01-07 16:13:23 +0400

    added rootpath to convert.py

--- airbnb/convert.py
+++ airbnb/convert.py
@@ -2,18 +2,21 @@ import click
 
 from airbnb import data
 from pathlib import Path
+import rootpath
+import pandas_utils as pu
 
+pu.install()
 
 @click.command()
 @click.argument("result_path")
 def convert(result_path: str):
-    project_path = Path(__file__).parent.parent
+    project_path = Path(rootpath.detect(__file__))
+
     input_file = project_path / f"results/{result_path}"
     output_file = project_path / f"results/{result_path}.pkl.zip"
+
     print(f"Converting {input_file} to {output_file}")
-    df = data.read_pickle_dir(input_file)
-    df1 = data.delete_duplicates(df)
-    df1.to_pickle(output_file)
+    df = data.read_pickle_dir(input_file).drop_duplicates(subset="listing_id").to_pickle(output_file)
 
 
 if __name__ == "__main__":
